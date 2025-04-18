1efb35f11010fd668658f474491e48da7f5d0706
ian <ian@isilverstone.com>
2025-04-17 22:19:35 -0500

    chore: setup main entry point impot and exports and ensure main entry point in package.json is src/index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,21 @@
 * Removal or modification of this copyright notice is prohibited.
 */
 
+import DB from './DB.js';
+import BaseModel from './BaseModel.js';
+import * as schemaBuilder  from './utils/schemaBuilder.js';
+import * as validation from './utils/validation.js';
+
+export {
+  DB,
+  BaseModel,
+  schemaBuilder,
+  validation,
+};
+
+export default {
+  DB,
+  BaseModel,
+  schemaBuilder,
+  validation,
+};
