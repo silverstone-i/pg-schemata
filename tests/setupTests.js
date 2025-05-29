841e0fb0d29e8b229885073e5a012e3cb8df2c6b
ian <ian@isilverstone.com>
2025-05-28 20:27:16 -0400

    refactor(tests): remove global console silencing in setupTests.js

--- tests/setupTests.js
+++
@@ -1,7 +0,0 @@
-
-// Silence console.log and console.error globally in tests
-beforeAll(() => {
-  jest.spyOn(console, 'log').mockImplementation(() => {});
-  jest.spyOn(console, 'warn').mockImplementation(() => {});
-  jest.spyOn(console, 'error').mockImplementation(() => {});
-});
